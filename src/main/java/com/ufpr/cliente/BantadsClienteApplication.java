abf38af03bfee3f0c50ffe60f074eb023bbefde0
IamThiago-IT <thiagodossantos315@gmail.com>
2024-06-20 15:56:11 -0300

    feat: add files

---
+++ src/main/java/com/ufpr/cliente/BantadsClienteApplication.java
@@ -0,0 +1,13 @@
+package com.ufpr.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BantadsClienteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BantadsClienteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ufpr/cliente/BantadsClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpr.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BantadsClienteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
